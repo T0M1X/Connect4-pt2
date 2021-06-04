420e0afc030df4d7553c5eff7bc057e70a18bed8
VenksamaM <venksama@gmail.com>
2021-06-04 15:31:58 +0100

    Connect 4 base game completed
    Needs to be tested thoroughly still.

--- connect4.py
+++ connect4.py
@@ -1,1 +1,104 @@
-#main file for coding the game
+# main file for coding the game
+grid = [[0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0]]
+isGameRunning = True
+
+
+def printGrid():
+    for i in range(len(grid)):
+        print(grid[i])
+
+
+def isPossible(column):
+    if grid[0][column] == 0:
+        return True
+    else:
+        return False
+
+
+def placeCounter(playerID, column):
+    for i in range(0, len(grid)):
+        if i + 1 >= len(grid) or grid[i + 1][column] != 0:
+            grid[i][column] = playerID
+            checkForWins(playerID, i, column)
+            return
+
+
+def takeTurn(playerID):
+    column = int(input("Enter column: "))
+    if isPossible(column):
+        placeCounter(playerID, column)
+
+
+def countPieces(playerID, row, column, rowIncrement, columnIncrement):
+    counter = 0
+
+    try:
+        x = row
+        y = column
+        while grid[x][y] == playerID:
+            if counter >= 4:
+                return 4
+
+            counter += 1
+            x += rowIncrement
+            y += columnIncrement
+
+        x = row - rowIncrement
+        y = column - columnIncrement
+        while grid[x][y] == playerID:
+            if counter >= 4:
+                return 4
+
+            counter += 1
+            x -= rowIncrement
+            y -= columnIncrement
+
+        return counter
+    except:
+        return counter
+
+
+def winSequence(playerID):
+    global isGameRunning
+
+    printGrid()
+    print(playerID, " wins!!!")
+    isGameRunning = False
+
+
+def checkForWins(playerID, row, column):
+    # horizontal
+    if countPieces(playerID, row, column, 1, 0) == 4:
+        winSequence(playerID)
+
+    # vertical
+    if countPieces(playerID, row, column, 0, -1) == 4:
+        winSequence(playerID)
+
+    # 45-degrees left
+    if countPieces(playerID, row, column, -1, -1) == 4:
+        winSequence(playerID)
+
+    # 45-degrees right
+    if countPieces(playerID, row, column, 1, -1) == 4:
+        winSequence(playerID)
+
+
+turnCounter = 0
+# game
+while isGameRunning:
+    printGrid()
+    if turnCounter % 2:
+        # player 2's turn
+        takeTurn(2)
+
+    else:
+        # player 1's turn
+        takeTurn(1)
+
+    turnCounter += 1
